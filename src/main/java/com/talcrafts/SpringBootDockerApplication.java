f68b50659c6c172d31a94a7f8ae627b21cbc6ba2
Imteyaz Ahmad <imteyaza-1lm@Imteyazs-MacBook-Pro.local>
2019-02-23 20:29:16 +0530

    refactored package name

--- src/main/java/com/talcrafts/SpringBootDockerApplication.java
+++ src/main/java/com/talcrafts/SpringBootDockerApplication.java
@@ -1,4 +1,4 @@
-package com.imti.springbootdocker;
+package com.talcrafts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/talcrafts/SpringBootDockerApplicationTests.java
+++ src/test/java/com/talcrafts/SpringBootDockerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.imti.springbootdocker;
+package com.talcrafts;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
